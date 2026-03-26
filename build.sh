65e2c7aaa1055f83d9d40ae62be9fbdd553f17fb
jagonzibwa <j.agonzibwa@alustudent.com>
2026-03-26 12:28:18 +0300

    creating a superuser

--- build.sh
+++ build.sh
@@ -4,3 +4,8 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+# Create superuser from environment variables (only if it doesn't already exist)
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ]; then
+    python manage.py createsuperuser --noinput || true
+fi
